c6472fee84d73f7ddd62283783401a3f4b994218
jingzing <louiswang0898@gmail.com>
2016-08-08 15:32:38 +0800

    mybaits 分页完成

--- src/main/java/tk/jingzing/web/mybatis/CountryController.java
+++ src/main/java/tk/jingzing/web/mybatis/CountryController.java
@@ -3,8 +3,11 @@ package tk.jingzing.web.mybatis;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import tk.jingzing.model.Country;
 import tk.jingzing.service.mybatis.CountryService;
 
@@ -36,6 +39,32 @@ public class CountryController {
     public ModelAndView view(){
         ModelAndView result = new ModelAndView("mybatis/view");
         result.addObject("country", new Country());
+        return result;
+    }
+
+    @RequestMapping(value = "/view/{id}")
+    public ModelAndView view(@PathVariable Integer id) {
+        ModelAndView result = new ModelAndView("mybatis/view");
+        Country country = countryService.getById(id);
+        result.addObject("country", country);
+        return result;
+    }
+
+    @RequestMapping(value = "/delete/{id}")
+    public ModelAndView delete(@PathVariable Integer id, RedirectAttributes ra) {
+        ModelAndView result = new ModelAndView("redirect:/countries");
+        countryService.deleteById(id);
+        ra.addFlashAttribute("msg", "删除成功!");
+        return result;
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ModelAndView save(Country country){
+        ModelAndView result = new ModelAndView("mybatis/view");
+        String msg = country.getId() == null ? "新增成功!" : "更新成功!";
+        countryService.save(country);
+        result.addObject("country", country);
+        result.addObject("msg", msg);
         return result;
     }
 }
