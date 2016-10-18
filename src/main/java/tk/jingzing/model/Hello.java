de35a33045c49ec5f3a42579241aebf24991545b
jingzing <louiswang0898@gmail.com>
2016-10-18 14:13:15 +0800

    rest versioning hello test

---
+++ src/main/java/tk/jingzing/model/Hello.java
@@ -0,0 +1,11 @@
+package tk.jingzing.model;
+
+/**
+ * versioning 包use
+ * Created by wangyunjing on 2016/10/18.
+ */
+public class Hello {
+    public String getHello() {
+        return "world";
+    }
+}
--- src/main/java/tk/jingzing/web/versioning/HelloController.java
+++ src/main/java/tk/jingzing/web/versioning/HelloController.java
@@ -1,6 +1,14 @@
 package tk.jingzing.web.versioning;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tk.jingzing.model.Hello;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 
 /**
  * How to implement the different approaches at REST API versioning with Spring MVC
@@ -14,4 +22,23 @@ public class HelloController {
         v1,
         v2
     }
+
+    @ResponseBody
+    @RequestMapping(value = "/apiurl/{version}/hello", method = GET, produces = APPLICATION_JSON_VALUE)
+    public Hello sayHelloWorldUrl(@PathVariable final ValidVersion version){
+        return new Hello();
+    }
+
+    /*@ResponseBody
+    @RequestMapping(value = "/apiheader/hello", method = GET, produces = APPLICATION_JSON_VALUE)
+    public Hello sayHelloWorldHeader(@RequestHeader("X-API-Version") final ValidVersion version){
+        return new Hello();
+    }
+
+    @ResponseBody
+    @RequestMapping( value = "/apiaccept/hello", method = GET,
+                    produces = {"application/vnd.company.app-v1+json","application/vnd.company.app-v2+json\""})
+    public Hello sayHelloWorldAccept(){
+        return new Hello();
+    }*/
 }
